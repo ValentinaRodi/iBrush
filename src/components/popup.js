f50bf49874827e789b2fa139635209ee7697c596
Родионова Валентина <rodionova.v@x3group.ru>
2023-10-13 14:55:47 +0500

    add files

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,6 +1,9 @@
+import '../images/angle-arrow-up.png';
+
 const form = document.querySelector('.form');
-const popupBtn = form.querySelector('.info__input_btn');
+const popupBtn = form.querySelector('.form__select');
 const popup = form.querySelector('.form__popup');
+const popupText = form.querySelector('.select-text');
 const arrow = form.querySelector('.btn__arrow');
 let displayNone = true;
 
@@ -10,18 +13,17 @@ popupBtn.addEventListener('click', (event) => {
         popup.classList.remove('display-none');
         popup.classList.add('display-block');
         displayNone = false;
-        arrow.src = "./images/angle-arrow-up.png";
+        arrow.src = "images/angle-arrow-up.png";
     }  else {
         popup.classList.remove('display-block');
         popup.classList.add('display-none');
         displayNone = true;
-        arrow.src = "./images/angle-arrow-down.png";
+        arrow.src = "images/angle-arrow-down.png";
     }
 });
 
 popup.addEventListener('click', (event) => {
-    console.log(popupBtn.data)
-    popupBtn.innerHTML = event.target.id;
+    popupText.innerHTML = event.target.id;
     popupBtn.classList.add('popup__book_text');
 });
 
